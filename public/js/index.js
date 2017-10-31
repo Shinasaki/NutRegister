a4233143bc07519f8067ec6ec78ee5f3993b0817
Shinasaki <Shinasaki07@gmail.com>
2017-10-31 15:54:15 +0700

    (Source) Peaches Update

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,17 @@
-var db = {}
+$(function(){
+    $('.form-signin').submit(function(event) {
+        // Stop event (html redirect when form submit)
+        event.preventDefault()
+        
+        // Get form data
+        var formData = $(this).serialize()
 
-$(document).ready(function() {
-   $('select').material_select();
-   $('#submit').click(send);
-   for (var i = 0; i < db.length; i++) {
-       $('.container').append('<div></div>')
-   }
-});
+        exportData(formData)
+    })
+})
 
-function getData() {
-    var lst = ['name', 'email', 'number', 'social', 'career', 'contact', 'when']
-    var except = ['image']
-    var data = {}
-    for (var i = 0; i < lst.length; i++) {
-        // console.log(lst[i]);
-        data[lst[i]] = document.getElementById(lst[i]).value
-    }
-    // console.log(data);
-    return data
-}
-
-function send() {
-    var data = getData()
-    $.post('/check',  data, function (result) {
-        db = result
-        console.log(resuldt);
-    }, 'json');
+function exportData(data) {
+    $.post('/check', data, function (result) {
+        console.log('send data to /check success!')
+    })
 }
--- server.js
+++ server.js
@@ -4,18 +4,39 @@ var bodyParser = require("body-parser");
 var Datastore = require('nedb')
 var db = new Datastore({ filename: 'data/database.db', autoload: true })
 
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM(`<!DOCTYPE html>`);
+const $ = require('jQuery')(window);
+
+
+
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
 
 app.post('/check', function(req, res){
-    // console.log(req.body);
+    
     db.insert(req.body, function(err, doc) {
-        console.log('Inserted');
-    });
+        console.log('Data inserted!')
+    })
+    
     db.find({}, function (err, docs) {
-        // console.log(docs)
-        res.json(docs)
-    });
+        // var display = $('.display')
+        $.each(docs, function(key, value) { // idk how to send data to view
+            console.log('ID: ' + key)
+            console.log('Email: ' + value.email + ' Name: ' + value.name) 
+            
+
+            // $('.display').html(
+            //     '<div class="box">' +
+            //         '<div class="col s3">ID: ' + key + '</di>' +
+            //         '<div class="col s3">Email: ' + value.email + '</div>' +
+            //         '<div class="col s3">Tel: ' + value.tel + '</div>' + 
+            //     '</div>'
+            // )
+        })
+    })
 })
 
 app.listen(3000, function () {
